dba0550f0f15996308e44e647e5577b644a55705
Martin Echeverria <martin.echeverria@accenture.com>
2025-06-15 20:47:25 -0300

    Init repo

---
+++ README.md
@@ -0,0 +1,6 @@
+Para arrancar Ionic en local:
+ionic serve --watch
+
+Para compilar el proyecto antes de Abrir Android:
+	1- ionic capacitor build android
+	2- npx capacitor-assets generate --android
---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { Device } from '@capacitor/device';
+import { Platform } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
+import { SqlliteManagerService } from './services/sqllite-manager.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  standalone: false,
+})
+export class AppComponent {
+
+  public isWeb: boolean;
+  public load: boolean;
+
+  constructor( 
+    private translate: TranslateService,
+    private platform: Platform,
+    private sqliteService: SqlliteManagerService
+  ) {
+
+    this.translate.setDefaultLang('es');
+    this.isWeb = false;
+    this.load = false;
+    this.initApp();
+  }
+
+  initApp(){
+    this.platform.ready().then( async() => {
+      const lang = await Device.getLanguageCode();
+      const info = await Device.getInfo();
+
+      this.isWeb = info.platform == 'web';
+      
+      if (lang.value){
+        this.translate.use(lang.value.slice(0, 2));
+      }
+
+      this.sqliteService.init();
+      this.sqliteService.dbReady.subscribe(isReady => {
+          this.load = isReady;
+      });
+    });
+  }
+}
