7502ddde7c0006c8dc6c596e83e0b65a6a001370
Martin Echeverria <martin.echeverria@accenture.com>
2025-09-21 13:58:00 -0300

    Add capacitor notifications

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,6 +3,7 @@ import { SqlliteManagerService } from '../services/sqllite-manager.service';
 import { Log } from '../models/log';
 import { AlertService } from '../services/alert.service';
 import { TranslateService } from '@ngx-translate/core';
+import { LocalNotifications } from '@capacitor/local-notifications';
 
 @Component({
   selector: 'app-tab1',
@@ -61,6 +62,10 @@ export class Tab1Page implements OnInit {
         this.translate.instant('label.success'),
         this.translate.instant('label.success.message.add.record')
       );
+      if (!this.isLogging){
+        console.log('Envio notificacion');
+        this.scheduleEndOfDayNotification();
+      } 
       this.isLogging = !this.isLogging;
       this.getLastLogs();
       console.log(this.lastLogs);
@@ -81,18 +86,42 @@ export class Tab1Page implements OnInit {
   getTimeDiff(currentLog: Log): string | null {
     const index = this.lastLogs.indexOf(currentLog);
     if (index < 0 || index >= this.lastLogs.length - 1) return null;
-  
+
     // En DESC, el "inicio" está en el siguiente item
     const nextLog = this.lastLogs[index + 1];
     if (!nextLog || !nextLog.IsActive) return null;
-  
+
     const start = new Date(nextLog.Timestamp).getTime();
     const end = new Date(currentLog.Timestamp).getTime();
     const diffMs = end - start;
-  
+
     const hours = Math.floor(diffMs / 3600000);
     const minutes = Math.floor((diffMs % 3600000) / 60000);
-  
+
     return `${hours}h ${minutes}m`;
   }
+
+  async scheduleEndOfDayNotification() {
+    const now = new Date();
+    const endOfDay = new Date();
+    endOfDay.setHours(23, 59, 0, 0);
+    now.setMinutes(now.getMinutes() + 1);
+
+    // Solo si todavía falta para las 23:59
+    //if (endOfDay > now) {
+      await LocalNotifications.schedule({
+        notifications: [
+          {
+            id: 1,
+            title: 'Registro activo',
+            body: 'Tenés un registro activo, cerralo antes de las 00:00.',
+            schedule: { at: now },
+            sound: null,
+            smallIcon: 'ic_stat_icon_config_sample', // opcional
+          },
+        ],
+      });
+    //}
+  }
+
 }
