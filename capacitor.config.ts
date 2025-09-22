9f0cd8af48cffe6692f66236e349a8b889ed0393
Martin Echeverria <martin.echeverria@accenture.com>
2025-09-22 16:05:00 -0300

    Add build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Time Logger',
-  webDir: 'www',
+  webDir: 'dist/timeLogger',
   plugins: {
     CapacitorSQLite: {
       iosDatabaseLocation: 'Library/CapacitorDatabase',
